6d52fd98b41ffcb22a57be58ad75f70e214b99be
GSGunN <drift_king_gun@hotmail.com>
2019-02-11 19:57:21 +0700

    Create form-report.php

---
+++ test/form-report.php
@@ -0,0 +1,31 @@
+<br><center><h1>แจ้งปัญหาการใช้งาน</h1></center>
+<br>
+<form>
+&emsp;*บริการที่ต้องการแจ้ง : 
+<select>
+	<option value = "choose">กรุณาเลือกบริการ</option>
+	<option value = "choose">บริการอินเตอร์เนตความเร็วสูง ADSL</option>
+	<option value = "choose">โทรศัพท์ประจำที่และบริการ SPC</option>
+	<option value = "choose">โทรศัพท์สาธารณะ</option>
+	<option value = "choose">บริการ FTTx</option>
+	<option value = "choose">บริการ WiNet</option>
+	<option value = "choose">Circuit / Leases Line</option>
+	<option value = "choose">TOT3G</option>
+</select>
+
+<br>&emsp;*เลขหมาย / วงจร : <input type = "text" id = "id"><br>
+
+<br>&emsp;*ปัญหาการใช้งานเบื้องต้น / Service : <input type = "text" id = "id"><br>
+
+<br>&emsp;ปัญหาการใช้งานที่พบอื่นๆ : <input type = "text" id = "id"><br>
+
+<br>&emsp;*หมายเลขติดต่อกลับ : <input type = "text" id = "id"><br>
+
+<br>&emsp;* จำเป็นต้องกรอกข้อมูล
+<br>&emsp;* หมายเหตุ กรุณาเปิดโมเดม เพื่อเจ้าหน้าที่สามารถตรวจสอบได้
+
+<br><br>
+	<input type = "reset" value = "เคลียร์">&emsp;
+	<input type = "submit" value = "ยกเลิก">&emsp;&emsp;
+	<input type = "submit" value = "บันทึก">
+</form>
